bdcc09f04c6e6f447ba83f2a850cb5b08067b238
nowaynora <phuocmai190@gmail.com>
2024-07-15 19:57:56 +0700

    Slide Image

---
+++ page/exjs01.php
@@ -0,0 +1,9 @@
+<h4>Slide Image</h4>
+<div id="DivSlideimgage">
+    <div id="DivView">
+        <img src="" alt=""/>
+    </div>
+    <div id="DivList">
+        <img src="" alt=""/>
+    </div> 
+</div>
